22920ca0562bfd6466a9ec1726057a36a5ae2d16
MaccabiDev <37658179+MaccabiDev@users.noreply.github.com>
2022-10-29 14:30:26 +0300

    add server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const jsonMock = {
+  first_name: "Bob",
+  last_name: "Smith",
+  age: 88,
+  email: "example@mock.com",
+};
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/users", (req, res) => {
+  res.send(jsonMock);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch("http://localhost:3001/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {users != null ? (
+        <div className="card text-center m-3">
+          <h5 className="card-header">GET Users Request</h5>
+          <div className="card-body">User first name: {users.first_name}</div>
+          <div className="card-body">User last name: {users.last_name}</div>
+          <div className="card-body">User age: {users.age}</div>
+          <div className="card-body">User email: {users.email}</div>
+        </div>
+      ) : null}
+    </>
   );
 }
 
